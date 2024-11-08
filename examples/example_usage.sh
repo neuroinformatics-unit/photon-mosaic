805448465548bfcf1b9fc404190d71f59891d3ba
lauraporta <ucqflpo@ucl.ac.uk>
2024-11-08 16:04:09 +0000

    Change folder

--- examples/example_usage.sh
+++ examples/example_usage.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 python ./examples/example_usage.py \
-    /nfs/winstor/margrie/SimonWeiler/RawData/Invivo_imaging/3photon_rotation/shared/ \
+    /nfs/winstor/margrie/SimonWeiler/RawData/Invivo_imaging/3photon_rotation/ \
     /ceph/margrie/laura/cimaut/ \
     --folder_read_pattern '2*' \
