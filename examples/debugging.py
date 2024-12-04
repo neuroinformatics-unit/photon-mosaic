5a892140dbcc169d12d8021b793954131652987e
lauraporta <ucqflpo@ucl.ac.uk>
2024-12-04 18:12:22 +0000

    Update debugging script

--- examples/debugging.py
+++ examples/debugging.py
@@ -1,12 +1,26 @@
+import shutil
 from pathlib import Path
 
-from calcium_imaging_automation.core.pipeline import pipeline
+from derotation.analysis.metrics import stability_of_most_detected_blob
+from derotation.derotate_batch import derotate
 
-pipeline(
+from calcium_imaging_automation.core.pipeline import orchestrator
+
+try:
+    shutil.rmtree("/ceph/margrie/laura/cimaut/derivatives/")
+    shutil.rmtree("/ceph/margrie/laura/cimaut/submitit/")
+except FileNotFoundError:
+    print("No derivatives folder found")
+
+orchestrator(
     raw_data_path=Path(
         "/nfs/winstor/margrie/SimonWeiler/RawData/Invivo_imaging/3photon_rotation/shared/"
     ),
     output_path=Path("/ceph/margrie/laura/cimaut/"),
     folder_read_pattern="2*",
     file_read_pattern=["rotation_00001.tif", "*.bin"],
+    experiment_name="submitit_04",
+    preprocessing_function=derotate,
+    compute_metric=stability_of_most_detected_blob,
+    # suite2p_ops_path="/ceph/margrie/laura/derotation/suite2p/laura_ops.npy",
 )
