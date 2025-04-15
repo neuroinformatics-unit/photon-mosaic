ff76ebb78cffc7463471cd24dc5434b82e915696
lauraporta <ucqflpo@ucl.ac.uk>
2025-04-15 17:01:51 +0100

    Add docstings

--- photon_mosaic/rules/suite2p_run.py
+++ photon_mosaic/rules/suite2p_run.py
@@ -1,14 +1,41 @@
 import traceback
 from pathlib import Path
+from typing import Optional
 
 from suite2p import run_s2p
 
 from photon_mosaic.s2p_options import get_edited_options
 
 
-def run_suite2p(tiff_file, stat_path, bin_path, dataset_folder, user_ops_dict):
+def run_suite2p(
+    tiff_file: str,
+    stat_path: str,
+    bin_path: str,
+    dataset_folder: Path,
+    user_ops_dict: Optional[dict] = None,
+):
+    """
+    This function runs Suite2P on a given TIFF file and saves the
+    results in the specified paths. It also handles any exceptions
+    that may occur during the process and logs them in an error
+    file.
+
+    Parameters
+    ----------
+    tiff_file : str
+        The path to the TIFF file to be processed.
+    stat_path : str
+        The path where the Suite2P statistics will be saved.
+    bin_path : str
+        The path where the Suite2P binary data will be saved.
+    dataset_folder : str
+        The path to the folder containing the dataset.
+    user_ops_dict : dict, optional
+        A dictionary containing user-provided options to override
+        the default Suite2P options. The default is None.
+    """
     save_folder = Path(stat_path).parent
-    dataset_folder = Path(dataset_folder)
+
     ops = get_edited_options(
         input_path=dataset_folder,
         save_folder=save_folder,
--- photon_mosaic/s2p_options.py
+++ photon_mosaic/s2p_options.py
@@ -1,7 +1,39 @@
+from pathlib import Path
+from typing import Optional
+
 from suite2p import default_ops
 
 
-def get_edited_options(input_path, save_folder, user_ops_dict=None):
+def get_edited_options(
+    input_path: Path, save_folder: Path, user_ops_dict: Optional[dict] = None
+) -> dict:
+    """This function generates a dictionary of options for Suite2P
+    by loading the default options and then modifying them with
+    user-provided options. The function also sets the required
+    runtime paths for saving the results.
+
+    Parameters
+    ----------
+    input_path : Path
+        The path to the input data folder.
+    save_folder : Path
+        The path to the folder where the results will be saved.
+    user_ops_dict : dic, optional
+        A dictionary containing user-provided options to override
+        the default options. The default is None.
+
+    Returns
+    -------
+    dict
+        A dictionary containing the Suite2P options, including
+        the user-provided options and the required runtime paths.
+
+    Raises
+    ------
+    ValueError
+        If a user-provided option is not valid for Suite2P.
+    """
+
     ops = default_ops()
 
     # Override with user-provided subset of keys
